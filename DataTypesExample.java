c359fc4c037494c677655cc9c42722ba41d630f7
shruti042006 <shruti.2403294203@vcet.edu.in>
2025-09-18 21:16:11 +0530

    Add files via upload

---
+++ DataTypesExample.java
@@ -0,0 +1,17 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int age = 20;           // integer
+        float price = 99.99f;   // floating-point
+        double pi = 3.14159;    // double precision
+        char grade = 'A';       // character
+        boolean isJavaFun = true; // boolean
+        long bigNumber = 123456789L; // long integer
+
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Pi: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+        System.out.println("Big Number: " + bigNumber);
+    }
+}
