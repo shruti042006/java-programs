4f3e7fc0c78d1d1a796cbba6a40099f752791a6f
shruti042006 <shruti.2403294203@vcet.edu.in>
2025-09-18 21:17:42 +0530

    Add files via upload

---
+++ BooleanOperators.java
@@ -0,0 +1,9 @@
+public class BooleanOperators {
+    public static void main(String[] args) {
+        boolean x = true, y = false;
+
+        System.out.println("x && y: " + (x && y)); // AND
+        System.out.println("x || y: " + (x || y)); // OR
+        System.out.println("!x: " + (!x));         // NOT
+    }
+}
