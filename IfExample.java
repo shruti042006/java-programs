004fe6b8bef6ed886e0053764d14a9fd635264ad
shruti042006 <shruti.2403294203@vcet.edu.in>
2025-09-23 22:25:50 +0530

    Add files via upload

---
+++ IfExample.java
@@ -0,0 +1,8 @@
+public class IfExample {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
+    }
+}
